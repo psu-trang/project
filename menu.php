0222db24fb15280541ed223d519d66a4f80becbf
pongsakron0904 <49774581+pongsakron0904@users.noreply.github.com>
2019-04-19 11:47:18 +0700

    Add files via upload

---
+++ menu.php
@@ -0,0 +1,62 @@
+
+<div class ="container-fluid"> 
+          <nav class="navbar navbar-expand-lg navbar-dark bg-danger fixed-top">
+              <a class="navbar-brand text-white">ร้านรักไทยซุปเปอร์มาร์เก็ต</a>
+              <button class="navbar-toggler" data-target="#my-nav" data-toggle="collapse">
+                  <span class="navbar-toggler-icon"></span>
+              </button>
+              <div id="my-nav" class="collapse navbar-collapse">
+                  <ul class="navbar-nav mr-auto">
+                      <li class = "nav-item dropdown" >
+                          <a href = "#"  class = "nav-link dropdown-toggle" data-toggle = "dropdown">ผู้ใช้งาน</a>
+                          <div class="dropdown-menu">
+                                  <a class="dropdown-item" href="Module1/datafrom.php">ดูรายชื่อ</a>
+                                  <a class="dropdown-item" href="Module1/adminfrom.php">เพิ่มเจ้าของร้าน</a>
+                                  <a class="dropdown-item" href="Module1/stafffrom.php">เพิ่มพนักงาน</a>
+                                  <a class="dropdown-item" href="Module1/supplierfrom.php">เพิ่มตัวแทนจำหน่าย</a>
+								  <a class="dropdown-item" href="Module1/customerfrom.php">เพิ่มลูกค้า</a>
+                          </div>
+                      </li>
+
+
+                  <li class = "nav-item"><a href = "Module2/order.php"  class = "nav-link active">ซื้อ </a></li>
+                  <li class = "nav-item dropdown" >
+                      <a href = "#"  class = "nav-link dropdown-toggle" data-toggle = "dropdown">ขาย</a>
+                      <div class = "dropdown-menu">
+                          <a href ="Module3/sellfrom.php" class ="dropdown-item"> รายการขาย</a>
+                          <a href ="Module3/receiptfrom.php" class ="dropdown-item"> ใบเสร็จเก่า</a>
+                      </div>
+                  </li>
+                  <li class = "nav-item dropdown" >
+                      <a href = "#"  class = "nav-link dropdown-toggle" data-toggle = "dropdown">สินค้า</a>
+                      <div class = "dropdown-menu">
+                          <a href ="Module4/productfrom.php" class ="dropdown-item"> กลุ่มสินค้า</a>
+                          <a href ="Module4/stockfrom.php" class ="dropdown-item"> จัดการสต๊อก</a>
+                          <a href ="Module4/totalfrom.php" class ="dropdown-item"> สินค้าคงเหลือ</a>
+                      </div>
+                  </li>
+
+                  <li class = "nav-item dropdown" >
+                      <a href = "#"  class = "nav-link dropdown-toggle" data-toggle = "dropdown">ออกรายงาน</a>
+                      <div class = "dropdown-menu">
+                          <a href ="Module5/sell report from.php" class ="dropdown-item"> รายงานการขาย</a>
+                          <a href ="Module5/order report from.php" class ="dropdown-item"> รายงานสั่งซื้อ</a>
+                          <a href ="Module5/stock report from.php" class ="dropdown-item"> รายงานสต๊อกสินค้า</a>
+                      </div>
+                  </li>
+          </ul>
+      </div>
+	  
+				<div id="my-nav" class="collapse navbar-collapse">
+                        <ul class="navbar-nav ml-auto">
+						 <li class = "nav-item dropdown" >
+                                <a href = "#"  class = "nav-link dropdown-toggle" data-toggle = "dropdown">ยินดีต้อนรับคุณ ........</a>
+                                <div class="dropdown-menu">
+                                        <a class="dropdown-item" href="index.php">ออกจากระบบ</a>
+                                 </div> 
+                            </li>
+								
+							</ul>
+						</div>			
+  </nav>
+</div>
